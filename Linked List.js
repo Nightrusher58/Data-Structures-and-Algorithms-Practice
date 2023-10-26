0e6c81c0e8da8835ccd29210e945feb26abbbc1c
unknown <tobilobadurojaiye@gmail.com>
2023-10-25 23:14:06 -0400

    unilist

--- Linked List.js
+++ Linked List.js
@@ -167,3 +167,20 @@
 
 //   return dummyStart.next;
 // };
+
+// Write a function, isUnivalueList, that takes in the head of a linked list as an argument. The function should return a boolean indicating whether or not the linked list contains exactly one unique value.
+
+// You may assume that the input list is non-empty.
+
+// const isUnivalueList = (head) => {
+//   let current = head;
+//   while (current !== null) {
+//     if (current.val !== head.val) {
+//       return false;
+//     }
+//     current = current.next;
+//   }
+//   if (current === null) {
+//     return true;
+//   }
+// };
