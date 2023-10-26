76e2f8d78508dc83f95e764c50c67eed9c81ead6
unknown <tobilobadurojaiye@gmail.com>
2023-10-25 22:33:45 -0400

    Merge List

--- Linked List.js
+++ Linked List.js
@@ -140,3 +140,30 @@
 //   if (current2 !== null) tail.next = current2
 //   return head1
 // };
+
+// Write a function, mergeLists, that takes in the head of two sorted linked lists as arguments. The function should merge the two lists together into single sorted linked list. The function should return the head of the merged linked list.
+
+// Do this in-place, by mutating the original Nodes.
+
+// You may assume that both input lists are non-empty and contain increasing sorted numbers.
+
+// const mergeLists = (head1, head2) => {
+//   let dummyStart = new Node(null);
+//   let current1 = head1;
+//   let current2 = head2;
+//   let tail = dummyStart;
+//   while (current1 !== null && current2 !== null) {
+//     if (current1.val < current2.val) {
+//       tail.next = current1;
+//       current1 = current1.next;
+//     } else {
+//       tail.next = current2;
+//       current2 = current2.next;
+//     }
+//     tail = tail.next;
+//   }
+//   if (current1 !== null) tail.next = current1;
+//   if (current2 !== null) tail.next = current2;
+
+//   return dummyStart.next;
+// };
