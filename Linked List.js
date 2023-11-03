4812a8ee4b8f35c42567fa8380377238bb94f2db
unknown <tobilobadurojaiye@gmail.com>
2023-11-03 16:25:03 -0400

    Depth First search (stack)

--- Linked List.js
+++ Linked List.js
@@ -235,3 +235,15 @@
 //   }
 //   return head;
 // };
+
+// Write a function, createLinkedList, that takes in an array of values as an argument. The function should create a linked list containing each element of the array as the values of the nodes. The function should return the head of the linked list.
+
+// const createLinkedList = (values) => {
+//   let dummyHead = new Node(null);
+//   let tail = dummyHead;
+//   for (items of values) {
+//     tail.next = new Node(items);
+//     tail = tail.next;
+//   }
+//   return dummyHead.next;
+// };
