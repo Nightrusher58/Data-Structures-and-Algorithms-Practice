8ee92039b720d05bf96003352591a72abf115629
unknown <tobilobadurojaiye@gmail.com>
2023-11-06 21:00:00 -0500

    depth first traversal

--- Binary Trees.js
+++ Binary Trees.js
@@ -1,2 +1,19 @@
 // Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
-test;
+
+// const depthFirstValues = (root) => {
+//   if (root === null) return []
+
+//   const stack = [root];
+//   let result = [];
+
+//   while (stack.length > 0){
+//     let current = stack.pop()
+//     result.push(current.val)
+
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+
+//   }
+
+//   return result;
+// };
