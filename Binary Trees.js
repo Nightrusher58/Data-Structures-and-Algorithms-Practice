b5460844f84b3c7b9e5bdc070c4c0ce5250cbbde
unknown <tobilobadurojaiye@gmail.com>
2023-11-07 23:04:21 -0500

    bfs

--- Binary Trees.js
+++ Binary Trees.js
@@ -17,3 +17,22 @@
 
 //   return result;
 // };
+
+// breath first trav
+// const breadthFirstValues = (root) => {
+//   if (root === null) return [];
+
+//   const values = [];
+//   const queue = [root];
+
+//   while (queue.length > 0) {
+//     const node = queue.shift();
+//     values.push(node.val);
+
+//     if (node.left !== null) queue.push(node.left);
+
+//     if (node.right !== null) queue.push(node.right);
+//   }
+
+//   return values;
+// };
